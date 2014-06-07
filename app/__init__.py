a45e163fe1f312cbba7e0f05b3297c3baf516ae3
sthoma <sthoma4@gmail.com>
2014-06-06 21:04:56 -0600

    Add base template.

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
+
 app = Flask(__name__)
 
+bootstrap = Bootstrap(app)
+
 @app.route("/")
-def hello():
-    return "Hello! Airworx Cinema"
+def index():
+    return render_template('base.html')
 
 if __name__ == "__main__":
     app.run()
